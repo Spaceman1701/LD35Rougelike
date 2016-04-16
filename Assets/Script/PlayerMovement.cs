ba214eff45811c1e824be283ccb6c958d002458f
Spaceman1701 <fc.spaceman@gmail.com>
2016-04-16 01:24:36 -0600

    Add random bullshit

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,28 +4,39 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
 	public float speed = 1.5f;
-	private Vector3 Target;
+	private Vector3 target;
 
 	private bool Moving;
 
 	void Start () 
 	{
-		Target = transform.position;
+		target = transform.position;
 	}
 
 	void Update () {
 		if (Input.GetMouseButton(1)) 
 		{
-			Target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			Target.z = transform.position.z;
+			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			target.z = transform.position.z;
 		}
+        RotateToTarget(target);
 		MoveUnit ();
 
 	}
 
 	void MoveUnit ()
 	{
-		transform.position = Vector3.MoveTowards(transform.position, Target, speed * Time.deltaTime);
-		Debug.DrawLine (transform.position, Target, Color.red);
+		transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+		Debug.DrawLine (transform.position, target, Color.red);
 	}
+
+    private void RotateToTarget(Vector3 target)
+    {
+        Vector3 direction = target - transform.position;
+        direction.Normalize();
+
+        float dir = Mathf.Atan2(direction.y, direction.x);
+
+        transform.rotation = Quaternion.AngleAxis(dir*Mathf.Rad2Deg + 90, Vector3.forward);
+    }
 }
