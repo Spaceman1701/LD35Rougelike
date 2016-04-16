6b0ec75ef8740bc233d5241c6fbc6fbf568833c6
xonasy <donahue.avila@gmail.com>
2016-04-16 15:59:03 -0500

    Updated Player Movement
    :)

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,13 +4,24 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
 	public float speed = 1.5f;
+    private int currImportance;
 	private Vector3 target;
+    private Rigidbody2D playerBody;
 
-	private bool Moving;
+    //Accessors
+    public Vector2 position;
+    public Vector2 velocity;
+    public float rotation;
+    public float angularVelocity;
+
+
+    private bool Moving;
 
 	void Start () 
 	{
 		target = transform.position;
+        currImportance = 0;
+        playerBody = GetComponent<Rigidbody2D>();
 	}
 
 	void Update () {
@@ -39,4 +50,90 @@ public class PlayerMovement : MonoBehaviour {
 
         transform.rotation = Quaternion.AngleAxis(dir*Mathf.Rad2Deg + 90, Vector3.forward);
     }
+
+
+    public void SetImportance(int j)
+    {
+        currImportance = j;
+    }
+
+    public bool SetPosition (Vector2 position, int importance)
+    {
+        if (importance >= currImportance)
+        {
+            currImportance = importance;
+            playerBody.position = position;
+            return true;
+        }
+        return false;
+    }
+
+    public bool SetVelocity (Vector2 velocity, int importance)
+    {
+        if (importance >= currImportance)
+        {
+            currImportance = importance;
+            playerBody.velocity = velocity;
+            return true;
+        }
+        return false;
+    }
+
+    public bool SetAngle(int angle, int importance)
+    {
+        if (importance >= currImportance)
+        {
+            currImportance = importance;
+            playerBody.rotation = angle;
+            return true;
+        }
+        return false;
+    }
+
+    public bool SetAngularVelocity(int angvelocity, int importance)
+    {
+        if (importance >= currImportance)
+        {
+            currImportance = importance;
+            playerBody.angularVelocity = angvelocity;
+            return true;
+        }
+        return false;
+    }
+
+    //Accessors
+    public Vector2 Position
+    {
+        get
+        {
+            return playerBody.position;
+        }
+    }
+
+    public Vector2 Velocity
+    {
+        get
+        {
+            return playerBody.velocity;
+        }
+    }
+
+    public float Rotation
+    {
+        get
+        {
+            return playerBody.rotation;
+        }
+    }
+
+    public float AngularVelocity
+    {
+        get
+        {
+            return playerBody.angularVelocity;
+        }
+    }
+
+
+
 }
