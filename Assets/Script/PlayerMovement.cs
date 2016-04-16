39786d2c6f91929ea04d6c59c08d16a87cd8bc29
aaaa9270 <aaaa9270@csgcnt.colorado.edu>
2016-04-16 00:45:17 -0600

    Beginnings of pathing
    I tried to get pathing to work but it didn't happen.  Player Movement
    works but not as intended.

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 1.5f;
+	private Vector3 Target;
+
+	private bool Moving;
+
+	void Start () 
+	{
+		Target = transform.position;
+	}
+
+	void Update () {
+		if (Input.GetMouseButton(1)) 
+		{
+			Target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Target.z = transform.position.z;
+		}
+		MoveUnit ();
+
+	}
+
+	void MoveUnit ()
+	{
+		transform.position = Vector3.MoveTowards(transform.position, Target, speed * Time.deltaTime);
+		Debug.DrawLine (transform.position, Target, Color.red);
+	}
+}
