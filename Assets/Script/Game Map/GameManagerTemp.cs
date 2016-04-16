a989b7d5fc200d2a3a38cb1f34d53f9b9e361e1b
Aaron Aaeng <aaaa9270@csgcnt.colorado.edu>
2016-04-16 13:37:13 -0600

    Basic Map Generator
    Added a basic map generator that places down tiles and walls.  There is
    no check to see whether the player will spawn trapped and it is
    technically possible for it to have impassable terrain.  #notmyproblem

---
+++ Assets/Script/Game Map/GameManagerTemp.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerTemp : MonoBehaviour {
+
+	public static GameManagerTemp instance = null;
+	public BoardManager boardScript;
+
+	private int level = 50;
+
+	void Awake()
+	{
+		if (instance == null) 
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy (gameObject);
+		}
+
+		DontDestroyOnLoad (gameObject);
+		boardScript = GetComponent<BoardManager> ();
+		InitGame ();
+	}
+
+	void InitGame()
+	{
+		boardScript.SetupScene(level);
+	}
+}
