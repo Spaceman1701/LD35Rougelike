38a147a113758f459d87b27d486137fe31d51f19
Aaron Aaeng <aaaa9270@csgcnt.colorado.edu>
2016-04-16 18:25:03 -0600

    Started the HUD
    Started to work on the hud.  It's pretty neat.

---
+++ Assets/Script/HUD/HealthBar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+	[SerializeField]
+	private RectTransform healthBarRect;
+	[SerializeField]
+	private Text healthText;
+
+	void Start () {
+		if (healthBarRect == null) 
+		{
+			Debug.LogError ("HealthBar.cs: No no health bar found");
+		}
+		if (healthText == null) 
+		{
+			Debug.LogError ("HealthBar.cs: No no health text found");
+		}
+	}
+	public void SetHealth(int cur, int max)
+	{
+		float value = (float)cur / max;
+
+		healthBarRect.localScale = new Vector3 (value, healthBarRect.localScale.y, healthBarRect.localScale.z);
+		healthText.text = cur + " / " + max;
+	}
+}
