907dda80d4bb0760492c523eddbaca9917e142d3
Spaceman1701 <fc.spaceman@gmail.com>
2016-04-16 21:18:34 -0600

    Mostly finish ability system
    Adds icons and descriptions to abilities

--- Assets/Script/Unit/Passive.cs
+++ Assets/Script/Unit/Passive.cs
@@ -3,8 +3,16 @@ using System.Collections;
 
 [RequireComponent(typeof(Unit))]
 public abstract class Passive : MonoBehaviour, ILevelable {
+    private const string DEFUALT_PASSIVE_FOLDER = "Sprite/passive/";
 
 	public int level;
+
+    private Sprite icon;
+
+    public void LoadIcon(string name)
+    {
+        icon = Resources.Load<Sprite>(DEFUALT_PASSIVE_FOLDER + name);
+    }
 
     public int Level
     {
@@ -16,6 +24,29 @@ public abstract class Passive : MonoBehaviour, ILevelable {
         {
             level = value;
         }
+    }
+
+    public abstract string Title
+    {
+        get;
+    }
+
+    public abstract string Description
+    {
+        get;
+    }
+
+    public Sprite Icon
+    {
+        get
+        {
+            return icon;
+        }
+    }
+
+    public bool IsIconLoaded()
+    {
+        return icon == null;
     }
 
 }
