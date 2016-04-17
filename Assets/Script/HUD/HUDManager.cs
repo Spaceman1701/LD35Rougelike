dbb90a2863b80b0a75a5e0f1a4db595939473b31
Aaron Aaeng <aaaa9270@csgcnt.colorado.edu>
2016-04-16 22:19:14 -0600

    No longer doing hud
    #notmyproblem
    
    There are a few changes.  Not super important

--- Assets/Script/HUD/HUDManager.cs
+++ Assets/Script/HUD/HUDManager.cs
@@ -3,9 +3,27 @@ using System.Collections;
 
 public class HUDManager : MonoBehaviour {
 
+	public GameObject playerGameObject;
+	public Texture EmptyAbility;
+
 	// Use this for initialization
 	void Start () {
-	
+		Ability[] abilities  = playerGameObject.GetComponentsInChildren<Ability>();
+		float[] abilityIconXLocations = new float[4] {(float)-206.33, (float)-37.8, (float)127.5, (float)296.0};
+		float abilityIconYLocation = (float)-424.74;
+
+		int length = abilities.Length;
+		for (int x = 0; x < 4; x++) 
+		{
+			if (abilities[x] != null)
+			{
+//				GUI.Button (new Rect(abilityIconXLocations[x], abilityIconYLocation, 150, 150), abilities[x].Icon); //THERE IS NO CURRENT TEXTURE ACCESSED THIS WAY
+			}
+			else 
+			{
+				GUI.Button (new Rect(abilityIconXLocations[x], abilityIconYLocation, 150, 150), EmptyAbility); 
+			}
+		}
 	}
 	
 	// Update is called once per frame
--- Assets/Script/HUD/ShowDescription.cs
+++ Assets/Script/HUD/ShowDescription.cs
@@ -1,11 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SpawnDescription : MonoBehaviour {
+public class ShowDescription : MonoBehaviour {
 
-	void onGUI()
+	public bool boolMouseOver = false ;
+	void OnMouseOver() // This Function for  Mouse Over a Mesh or GUIContent
 	{
-		Vector3 mouseLocation = Input.mousePosition;
-		GUI.Box (new Rect(mouseLocation.x, mouseLocation.y, 20, 20), "This is a test");
+		boolMouseOver = true;
+	}
+	void OnMouseExit() // This Function for  Mouse Not Over a Mesh or GUIContent
+	{
+		boolMouseOver = false;
+	}
+	void OnGUI()
+	{
+		GUI.depth = -10;
+
+		GUI.Button(new Rect(10, 10, 100, 20), new GUIContent("Click me", "Mouse Over Button")); // When Mouse Over this button GUI.tooltip = "Mouse Over Button"
+
+
+		if(GUI.tooltip =="Mouse Over Button")
+		{
+			boolMouseOver = true;
+		}
+		else
+		{
+			boolMouseOver = false;
+		}
+
+		if (boolMouseOver == true) {
+			GUI.Box (new Rect (500, 100, 100, 50), "Here is a box");
+
+			GUI.Box (new Rect (100, 100, 100, 50), "Here is a Button");
+		}
+	
 	}
 }
