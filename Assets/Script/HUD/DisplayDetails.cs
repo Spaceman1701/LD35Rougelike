e5ba553a4433f1dc176fdcc9222e7952e00a60da
Spaceman1701 <fc.spaceman@gmail.com>
2016-04-18 16:33:26 -0600

    Fix the fucking gui

--- Assets/Script/HUD/DisplayDetails.cs
+++ Assets/Script/HUD/DisplayDetails.cs
@@ -1,57 +1,97 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class DisplayDetails : MonoBehaviour {
+    public int abilityNum;
 
-	Ability[] abilities = new Ability[4];
+    public Ability ability;
 	int length;
 
 	int abilityIndex = -1;
-	public GameObject playerGameObject;
+	public PlayerUnit pu;
 
 	public int x=0;
 	public int y=0;
 
+    public Text cdText;
+    public Text infoText;
+
+    public bool details;
+
 	void Start()
 	{
-        PlayerUnit pu = playerGameObject.GetComponent<PlayerUnit>();
-		abilities = new Ability[] {pu.Ability1, pu.Ability2, pu.Ability3, pu.Ability4};
-		length = abilities.Length;
+        UpdateAbility();
 	}
 
-	public void showDetails(int index){
-		abilityIndex = index;
-	}
-
-    private void DrawAbilityText(int abilityIndex)
+	public void ShowDetails()
     {
-        if (abilities[abilityIndex] != null)
+        details = true;
+        if (details && ability != null)
         {
-            string abilityDesc = abilities[abilityIndex].Description;
-            string abilityTitle = abilities[abilityIndex].Title;
+            string abilityDesc = ability.Description;
+            string abilityTitle = ability.Title;
 
-            GUI.Label(new Rect(903, 523, 240, 30), abilityTitle);
-            GUI.Label(new Rect(903, 542, 240, 300), abilityDesc);
+            infoText.text =
+                abilityTitle + '\n' +
+                abilityDesc;
+        }
+    
+    }
+    
+
+    public void StopShowDetails()
+    {
+        details = false;
+        infoText.text = "";
+    }
+
+    private void UpdateAbility()
+    {
+        switch (abilityNum)
+        {
+            case 0:
+                ability = pu.Ability1;
+                break;
+            case 1:
+                ability = pu.Ability2;
+                break;
+            case 3:
+                ability = pu.Ability3;
+                break;
+            case 4:
+                ability = pu.Ability4;
+                break;
         }
     }
 
 	void OnGUI () {
+        UpdateAbility();
 //		GUI.Label (new Rect (x, y, 240, 30), "1.7");
 //		GUI.Label(new Rect(x, y, 240, 300), "asjfhadlfkhdaslfjkgdasljkgadsjlf");
-		length = abilities.Length;
         //Ability descriptions
-        if (abilityIndex != -1)
-        {
-            DrawAbilityText(abilityIndex);
+        
+        
 
+        if (ability != null && !ability.OffCooldown())
+        {
+            float cooldownTime = Mathf.Round(ability.cooldown * 100f) / 100f;
+            cdText.text = cooldownTime.ToString();
+        } else
+        {
+            cdText.text = "";
         }
+        
+        /*
         for (int i = 0; i < length; i++) {
 			if (abilities[i] != null && abilities [i].cooldown > 0){
 				float cooldownTime = abilities [i].cooldown;
 				cooldownTime = Mathf.Round (cooldownTime * 100f) / 100f;
 				if (i == 0)
-					GUI.Label (new Rect (488, 550, 240, 30), cooldownTime.ToString());
-				else if (i == 1)
+                    
+                    GUI.Label(new Rect(, 200, 30), cooldownTime.ToString());
+                //GUI.Label (new Rect (488, 550, 240, 30), cooldownTime.ToString());
+                else if (i == 1)
 					GUI.Label (new Rect (583, 550, 240, 30), cooldownTime.ToString());
 				else if (i == 2)
 					GUI.Label (new Rect (672, 550, 240, 30), cooldownTime.ToString());
@@ -61,6 +101,6 @@ public class DisplayDetails : MonoBehaviour {
 		}
 		GUI.contentColor = Color.black;
 		int level = playerGameObject.GetComponentInChildren<ILevelable> ().Level;
-		GUI.Label (new Rect (455, 602, 240, 300), level.ToString());
+		GUI.Label (new Rect (455, 602, 240, 300), level.ToString()); */
 	}
 }
