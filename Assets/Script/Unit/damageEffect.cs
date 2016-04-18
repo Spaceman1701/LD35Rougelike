991f0f0422716790049b130fa9d7493a10bd4349
xonasy <donahue.avila@gmail.com>
2016-04-18 00:14:15 -0500

    onDamageTaken listener
    ok

--- Assets/Script/Unit/damageEffect.cs
+++ Assets/Script/Unit/damageEffect.cs
@@ -1,12 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class damageEffect : MonoBehaviour{
+public class damageEffect : MonoBehaviour, DamageListener{
+
+    public SpriteRenderer[] spriteRenders;
+    public float damageColorTime;
 
     // Use this for initialization
-    void OnDamageTaken(float dmg)
+    public void OnDamageTaken(float dmg)
     {
         
+       
+        spriteRenders = GetComponentsInChildren<SpriteRenderer>();
+
+        foreach(SpriteRenderer r in spriteRenders)
+        {
+           r.color = Color.red;
+        }
+        Invoke("ResetColor", damageColorTime);
+
     }
+
+    private void ResetColor()
+    {
+        foreach (SpriteRenderer r in spriteRenders)
+        {
+            r.color = Color.white;
+        }
+    }
+
 
 }
