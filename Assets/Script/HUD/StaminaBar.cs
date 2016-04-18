123789cc728c005935d8a975c8e908a06df6f132
Aaron Aaeng <aaaa9270@colorado.edu>
2016-04-17 22:06:02 -0600

    Pushing hud
    deciding how to properly fix all of the hud things i had working before
    ethan decided to change the architecture

--- Assets/Script/HUD/StaminaBar.cs
+++ Assets/Script/HUD/StaminaBar.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 public class StaminaBar : MonoBehaviour {
 
 	[SerializeField]
-	private RectTransform staminaBarCover;
+	private RectTransform staminaBarRect;
 	[SerializeField]
 	private Text staminaText;
 	/*
@@ -21,9 +21,9 @@ public class StaminaBar : MonoBehaviour {
 	*/ //These were always showing even though it all worked properly.  Decided to just leave then in here and pretend like it never happened.  They won't look at this anyway
 	public void SetStamina(int cur, int max)
 	{
-		float value = 910 * (1- (float)cur / max);
+		float value = (float)cur / max;
 
-		staminaBarCover.localScale = new Vector3 (value, staminaBarCover.localScale.y, staminaBarCover.localScale.z);
+		staminaBarRect.localScale = new Vector3 (value, staminaBarRect.localScale.y, staminaBarRect.localScale.z);
 		staminaText.text = cur + " / " + max;
 	}
 }
