ea9f0a1b613ae0ea66d6d23c1d7fb0756aad371d
MLesartre <MLesartre@gmail.com>
2016-04-18 18:35:35 -0600

    Added Fireball

---
+++ Assets/Script/FireballProjectile.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballProjectile : MonoBehaviour {
+
+    public int damage;
+    public float startingLifetime;
+
+    float lifetime;
+    private const float LIFETIME_UPATE_RATE = 0.1f;
+
+    void Start()
+    {
+        lifetime = startingLifetime;
+        InvokeRepeating("DecreaseLifetime", 0, LIFETIME_UPATE_RATE);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.GetComponentInParent<EnemyUnit>())
+        {
+            other.GetComponentInParent<EnemyUnit>().Damage(damage);
+            CancelInvoke("DecreaseLifetime");
+            Destroy(gameObject);
+        }
+    }
+    private void DecreaseLifetime()
+    {
+        lifetime -= LIFETIME_UPATE_RATE;
+        if (lifetime <= 0.0)
+        {
+            CancelInvoke("DecreaseLifetime");
+            Destroy(gameObject);
+        }
+    }
+
+
+}
---
+++ Assets/Script/Unit/Abilities/FireballAbility.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class FireballAbility : Ability
+{
+    public GameObject fireball;
+    public override void OnCast()
+    {
+        if (OffCooldown() && GetComponentInParent<PlayerUnit>().UseStamina(staminaCost))
+        {
+            StartCooldown();
+            GameObject o = (GameObject)Instantiate(fireball,
+            new Vector3(playerUnit.RelativeOrigin.x, playerUnit.RelativeOrigin.y, 0), Quaternion.identity);
+            o.GetComponent<Rigidbody2D>().velocity = playerUnit.RelativeMouse.normalized * 10;
+        }
+    }
+
+    public override void ForceLoadIcon()
+    {
+        //Debug.Log("FORCE LOAD CALLED");
+        if (!IsIconLoaded())
+        {
+            loadIcon("skill_fireball");
+        }
+    }
+
+    public override string Title
+    {
+        get
+        {
+            return "Fireball";
+        }
+    }
+
+    public override string Description
+    {
+        get
+        {
+            return "Launches a fireball";
+        }
+    }
+}
